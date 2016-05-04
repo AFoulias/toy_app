89be1e7f744c37895b0d42ccd2d43a6b08a1616d
Alexander Foulias <alexander.foulias@gmail.com>
2016-05-04 17:08:29 +0000

    Finished Toy App!

---
+++ db/migrate/20160504164330_create_microposts.rb
@@ -0,0 +1,7 @@
+class CreateMicroposts < ActiveRecord::Migration
+  def change
+    remove_column :microposts, :user_id
+    remove_column :microposts, :integer
+    add_column :microposts, :user_id, :integer
+  end
+end
